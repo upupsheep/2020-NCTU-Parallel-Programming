75190ad3fb3c11d2add8649cfdb4292121e91579
upupsheep <sandy861003@gmail.com>
2020-11-19 00:43:47 +0800

    top down 4

--- hw3/part2/breadth_first_search/bfs.cpp
+++ hw3/part2/breadth_first_search/bfs.cpp
@@ -33,7 +33,7 @@ void top_down_step(
     int *distances) {
     int local_count = 0;
 
-#pragma omp parallel private(local_count)
+#pragma omp parallel {
 #pragma omp for reduction(+ \
                           : local_count)
     for (int i = 0; i < frontier->count; i++) {
@@ -57,6 +57,7 @@ void top_down_step(
             }
         }
     }
+}
 }
 
 // Implements top-down BFS.
