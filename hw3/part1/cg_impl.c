7b2f0625a7eecb4a1fe3d8c26a561aed07f77cd5
upupsheep <sandy861003@gmail.com>
2020-11-18 18:38:51 +0800

    parallelized by simply put pragma

--- hw3/part1/cg_impl.c
+++ hw3/part1/cg_impl.c
@@ -11,8 +11,7 @@ void conj_grad(int colidx[],
                double p[],
                double q[],
                double r[],
-               double *rnorm)
-{
+               double *rnorm) {
     int j, k;
     int cgit, cgitmax = 25;
     double d, sum, rho, rho0, alpha, beta;
@@ -22,8 +21,9 @@ void conj_grad(int colidx[],
     //---------------------------------------------------------------------
     // Initialize the CG algorithm:
     //---------------------------------------------------------------------
-    for (j = 0; j < naa + 1; j++)
-    {
+
+#pragma omp parallel for
+    for (j = 0; j < naa + 1; j++) {
         q[j] = 0.0;
         z[j] = 0.0;
         r[j] = x[j];
@@ -34,8 +34,10 @@ void conj_grad(int colidx[],
     // rho = r.r
     // Now, obtain the norm of r: First, sum squares of r elements locally...
     //---------------------------------------------------------------------
-    for (j = 0; j < lastcol - firstcol + 1; j++)
-    {
+
+#pragma omp parallel for reduction(+ \
+                                   : rho)
+    for (j = 0; j < lastcol - firstcol + 1; j++) {
         rho = rho + r[j] * r[j];
     }
 
@@ -44,8 +46,7 @@ void conj_grad(int colidx[],
     // The conj grad iteration loop
     //---->
     //---------------------------------------------------------------------
-    for (cgit = 1; cgit <= cgitmax; cgit++)
-    {
+    for (cgit = 1; cgit <= cgitmax; cgit++) {
         //---------------------------------------------------------------------
         // q = A.p
         // The partition submatrix-vector multiply: use workspace w
@@ -57,11 +58,13 @@ void conj_grad(int colidx[],
         //       unrolled-by-two version is some 10% faster.
         //       The unrolled-by-8 version below is significantly faster
         //       on the Cray t3d - overall speed of code is 1.5 times faster.
-        for (j = 0; j < lastrow - firstrow + 1; j++)
-        {
+
+#pragma omp parallel for private(sum)
+        for (j = 0; j < lastrow - firstrow + 1; j++) {
             sum = 0.0;
-            for (k = rowstr[j]; k < rowstr[j + 1]; k++)
-            {
+#pragma omp prallel for reduction(+ \
+                                  : sum)
+            for (k = rowstr[j]; k < rowstr[j + 1]; k++) {
                 sum = sum + a[k] * p[colidx[k]];
             }
             q[j] = sum;
@@ -71,8 +74,9 @@ void conj_grad(int colidx[],
         // Obtain p.q
         //---------------------------------------------------------------------
         d = 0.0;
-        for (j = 0; j < lastcol - firstcol + 1; j++)
-        {
+#pragma omp prallel for reduction(+ \
+                                  : d)
+        for (j = 0; j < lastcol - firstcol + 1; j++) {
             d = d + p[j] * q[j];
         }
 
@@ -91,8 +95,8 @@ void conj_grad(int colidx[],
         // and    r = r - alpha*q
         //---------------------------------------------------------------------
         rho = 0.0;
-        for (j = 0; j < lastcol - firstcol + 1; j++)
-        {
+#pragma omp parallel for
+        for (j = 0; j < lastcol - firstcol + 1; j++) {
             z[j] = z[j] + alpha * p[j];
             r[j] = r[j] - alpha * q[j];
         }
@@ -101,8 +105,10 @@ void conj_grad(int colidx[],
         // rho = r.r
         // Now, obtain the norm of r: First, sum squares of r elements locally...
         //---------------------------------------------------------------------
-        for (j = 0; j < lastcol - firstcol + 1; j++)
-        {
+
+#pragma omp parallel for reduction(+ \
+                                   : rho)
+        for (j = 0; j < lastcol - firstcol + 1; j++) {
             rho = rho + r[j] * r[j];
         }
 
@@ -114,11 +120,12 @@ void conj_grad(int colidx[],
         //---------------------------------------------------------------------
         // p = r + beta*p
         //---------------------------------------------------------------------
-        for (j = 0; j < lastcol - firstcol + 1; j++)
-        {
+
+#pragma omp parallel for
+        for (j = 0; j < lastcol - firstcol + 1; j++) {
             p[j] = r[j] + beta * p[j];
         }
-    } // end of do cgit=1,cgitmax
+    }  // end of do cgit=1,cgitmax
 
     //---------------------------------------------------------------------
     // Compute residual norm explicitly:  ||r|| = ||x - A.z||
@@ -126,11 +133,12 @@ void conj_grad(int colidx[],
     // The partition submatrix-vector multiply
     //---------------------------------------------------------------------
     sum = 0.0;
-    for (j = 0; j < lastrow - firstrow + 1; j++)
-    {
+#pragma omp parallel for
+    for (j = 0; j < lastrow - firstrow + 1; j++) {
         d = 0.0;
-        for (k = rowstr[j]; k < rowstr[j + 1]; k++)
-        {
+#pragma omp parallel for reduction(+ \
+                                   : d)
+        for (k = rowstr[j]; k < rowstr[j + 1]; k++) {
             d = d + a[k] * z[colidx[k]];
         }
         r[j] = d;
@@ -139,8 +147,10 @@ void conj_grad(int colidx[],
     //---------------------------------------------------------------------
     // At this point, r contains A.z
     //---------------------------------------------------------------------
-    for (j = 0; j < lastcol - firstcol + 1; j++)
-    {
+
+#pragma omp parallel for reduction(+ \
+                                   : sum)
+    for (j = 0; j < lastcol - firstcol + 1; j++) {
         d = x[j] - r[j];
         sum = sum + d * d;
     }
@@ -185,8 +195,7 @@ void makea(int n,
            int arow[],
            int acol[][NONZER + 1],
            double aelt[][NONZER + 1],
-           int iv[])
-{
+           int iv[]) {
     int iouter, ivelt, nzv, nn1;
     int ivc[NONZER + 1];
     double vc[NONZER + 1];
@@ -199,23 +208,20 @@ void makea(int n,
     // nn1 is the smallest power of two not less than n
     //---------------------------------------------------------------------
     nn1 = 1;
-    do
-    {
+    do {
         nn1 = 2 * nn1;
     } while (nn1 < n);
 
     //---------------------------------------------------------------------
     // Generate nonzero positions and save for the use in sparse.
     //---------------------------------------------------------------------
-    for (iouter = 0; iouter < n; iouter++)
-    {
+    for (iouter = 0; iouter < n; iouter++) {
         nzv = NONZER;
         sprnvc(n, nzv, nn1, vc, ivc);
         vecset(n, vc, ivc, &nzv, iouter + 1, 0.5);
         arow[iouter] = nzv;
 
-        for (ivelt = 0; ivelt < nzv; ivelt++)
-        {
+        for (ivelt = 0; ivelt < nzv; ivelt++) {
             acol[iouter][ivelt] = ivc[ivelt] - 1;
             aelt[iouter][ivelt] = vc[ivelt];
         }
@@ -247,8 +253,7 @@ void sparse(double a[],
             int lastrow,
             int nzloc[],
             double rcond,
-            double shift)
-{
+            double shift) {
     int nrows;
 
     //---------------------------------------------------
@@ -267,23 +272,19 @@ void sparse(double a[],
     //---------------------------------------------------------------------
     // ...count the number of triples in each row
     //---------------------------------------------------------------------
-    for (j = 0; j < nrows + 1; j++)
-    {
+    for (j = 0; j < nrows + 1; j++) {
         rowstr[j] = 0;
     }
 
-    for (i = 0; i < n; i++)
-    {
-        for (nza = 0; nza < arow[i]; nza++)
-        {
+    for (i = 0; i < n; i++) {
+        for (nza = 0; nza < arow[i]; nza++) {
             j = acol[i][nza] + 1;
             rowstr[j] = rowstr[j] + arow[i];
         }
     }
 
     rowstr[0] = 0;
-    for (j = 1; j < nrows + 1; j++)
-    {
+    for (j = 1; j < nrows + 1; j++) {
         rowstr[j] = rowstr[j] + rowstr[j - 1];
     }
     nza = rowstr[nrows] - 1;
@@ -292,8 +293,7 @@ void sparse(double a[],
     // ... rowstr(j) now is the location of the first nonzero
     //     of row j of a
     //---------------------------------------------------------------------
-    if (nza > nz)
-    {
+    if (nza > nz) {
         printf("Space for matrix elements exceeded in sparse\n");
         printf("nza, nzmax = %d, %d\n", nza, nz);
         exit(EXIT_FAILURE);
@@ -302,10 +302,8 @@ void sparse(double a[],
     //---------------------------------------------------------------------
     // ... preload data pages
     //---------------------------------------------------------------------
-    for (j = 0; j < nrows; j++)
-    {
-        for (k = rowstr[j]; k < rowstr[j + 1]; k++)
-        {
+    for (j = 0; j < nrows; j++) {
+        for (k = rowstr[j]; k < rowstr[j + 1]; k++) {
             a[k] = 0.0;
             colidx[k] = -1;
         }
@@ -318,15 +316,12 @@ void sparse(double a[],
     size = 1.0;
     ratio = pow(rcond, (1.0 / (double)(n)));
 
-    for (i = 0; i < n; i++)
-    {
-        for (nza = 0; nza < arow[i]; nza++)
-        {
+    for (i = 0; i < n; i++) {
+        for (nza = 0; nza < arow[i]; nza++) {
             j = acol[i][nza];
 
             scale = size * aelt[i][nza];
-            for (nzrow = 0; nzrow < arow[i]; nzrow++)
-            {
+            for (nzrow = 0; nzrow < arow[i]; nzrow++) {
                 jcol = acol[i][nzrow];
                 va = aelt[i][nzrow] * scale;
 
@@ -334,23 +329,18 @@ void sparse(double a[],
                 // ... add the identity * rcond to the generated matrix to bound
                 //     the smallest eigenvalue from below by rcond
                 //--------------------------------------------------------------------
-                if (jcol == j && j == i)
-                {
+                if (jcol == j && j == i) {
                     va = va + rcond - shift;
                 }
 
                 cont40 = false;
-                for (k = rowstr[j]; k < rowstr[j + 1]; k++)
-                {
-                    if (colidx[k] > jcol)
-                    {
+                for (k = rowstr[j]; k < rowstr[j + 1]; k++) {
+                    if (colidx[k] > jcol) {
                         //----------------------------------------------------------------
                         // ... insert colidx here orderly
                         //----------------------------------------------------------------
-                        for (kk = rowstr[j + 1] - 2; kk >= k; kk--)
-                        {
-                            if (colidx[kk] > -1)
-                            {
+                        for (kk = rowstr[j + 1] - 2; kk >= k; kk--) {
+                            if (colidx[kk] > -1) {
                                 a[kk + 1] = a[kk];
                                 colidx[kk + 1] = colidx[kk];
                             }
@@ -359,15 +349,11 @@ void sparse(double a[],
                         a[k] = 0.0;
                         cont40 = true;
                         break;
-                    }
-                    else if (colidx[k] == -1)
-                    {
+                    } else if (colidx[k] == -1) {
                         colidx[k] = jcol;
                         cont40 = true;
                         break;
-                    }
-                    else if (colidx[k] == jcol)
-                    {
+                    } else if (colidx[k] == jcol) {
                         //--------------------------------------------------------------
                         // ... mark the duplicated entry
                         //--------------------------------------------------------------
@@ -376,8 +362,7 @@ void sparse(double a[],
                         break;
                     }
                 }
-                if (cont40 == false)
-                {
+                if (cont40 == false) {
                     printf("internal error in sparse: i=%d\n", i);
                     exit(EXIT_FAILURE);
                 }
@@ -390,32 +375,25 @@ void sparse(double a[],
     //---------------------------------------------------------------------
     // ... remove empty entries and generate final results
     //---------------------------------------------------------------------
-    for (j = 1; j < nrows; j++)
-    {
+    for (j = 1; j < nrows; j++) {
         nzloc[j] = nzloc[j] + nzloc[j - 1];
     }
 
-    for (j = 0; j < nrows; j++)
-    {
-        if (j > 0)
-        {
+    for (j = 0; j < nrows; j++) {
+        if (j > 0) {
             j1 = rowstr[j] - nzloc[j - 1];
-        }
-        else
-        {
+        } else {
             j1 = 0;
         }
         j2 = rowstr[j + 1] - nzloc[j];
         nza = rowstr[j];
-        for (k = j1; k < j2; k++)
-        {
+        for (k = j1; k < j2; k++) {
             a[k] = a[nza];
             colidx[k] = colidx[nza];
             nza = nza + 1;
         }
     }
-    for (j = 1; j < nrows + 1; j++)
-    {
+    for (j = 1; j < nrows + 1; j++) {
         rowstr[j] = rowstr[j] - nzloc[j - 1];
     }
     nza = rowstr[nrows] - 1;
@@ -430,15 +408,13 @@ void sparse(double a[],
 // this corrects a performance bug found by John G. Lewis, caused by
 // reinitialization of mark on every one of the n calls to sprnvc
 //---------------------------------------------------------------------
-void sprnvc(int n, int nz, int nn1, double v[], int iv[])
-{
+void sprnvc(int n, int nz, int nn1, double v[], int iv[]) {
     int nzv, ii, i;
     double vecelt, vecloc;
 
     nzv = 0;
 
-    while (nzv < nz)
-    {
+    while (nzv < nz) {
         vecelt = randlc(&tran, amult);
 
         //---------------------------------------------------------------------
@@ -453,10 +429,8 @@ void sprnvc(int n, int nz, int nn1, double v[], int iv[])
         // was this integer generated already?
         //---------------------------------------------------------------------
         logical was_gen = false;
-        for (ii = 0; ii < nzv; ii++)
-        {
-            if (iv[ii] == i)
-            {
+        for (ii = 0; ii < nzv; ii++) {
+            if (iv[ii] == i) {
                 was_gen = true;
                 break;
             }
@@ -472,8 +446,7 @@ void sprnvc(int n, int nz, int nn1, double v[], int iv[])
 //---------------------------------------------------------------------
 // scale a double precision number x in (0,1) by a power of 2 and chop it
 //---------------------------------------------------------------------
-int icnvrt(double x, int ipwr2)
-{
+int icnvrt(double x, int ipwr2) {
     return (int)(ipwr2 * x);
 }
 
@@ -481,30 +454,25 @@ int icnvrt(double x, int ipwr2)
 // set ith element of sparse vector (v, iv) with
 // nzv nonzeros to val
 //---------------------------------------------------------------------
-void vecset(int n, double v[], int iv[], int *nzv, int i, double val)
-{
+void vecset(int n, double v[], int iv[], int *nzv, int i, double val) {
     int k;
     logical set;
 
     set = false;
-    for (k = 0; k < *nzv; k++)
-    {
-        if (iv[k] == i)
-        {
+    for (k = 0; k < *nzv; k++) {
+        if (iv[k] == i) {
             v[k] = val;
             set = true;
         }
     }
-    if (set == false)
-    {
+    if (set == false) {
         v[*nzv] = val;
         iv[*nzv] = i;
         *nzv = *nzv + 1;
     }
 }
 
-void init(double *zeta)
-{
+void init(double *zeta) {
     int i, j, k;
 
     firstrow = 0;
@@ -540,10 +508,8 @@ void init(double *zeta)
     //      Shift the col index vals from actual (firstcol --> lastcol )
     //      to local, i.e., (0 --> lastcol-firstcol)
     //---------------------------------------------------------------------
-    for (j = 0; j < lastrow - firstrow + 1; j++)
-    {
-        for (k = rowstr[j]; k < rowstr[j + 1]; k++)
-        {
+    for (j = 0; j < lastrow - firstrow + 1; j++) {
+        for (k = rowstr[j]; k < rowstr[j + 1]; k++) {
             colidx[k] = colidx[k] - firstcol;
         }
     }
@@ -551,12 +517,11 @@ void init(double *zeta)
     //---------------------------------------------------------------------
     // set starting vector to (1, 1, .... 1)
     //---------------------------------------------------------------------
-    for (i = 0; i < NA + 1; i++)
-    {
+    for (i = 0; i < NA + 1; i++) {
         x[i] = 1.0;
     }
-    for (j = 0; j < lastcol - firstcol + 1; j++)
-    {
+
+    for (j = 0; j < lastcol - firstcol + 1; j++) {
         q[j] = 0.0;
         z[j] = 0.0;
         r[j] = 0.0;
@@ -564,8 +529,7 @@ void init(double *zeta)
     }
 }
 
-void iterate(double *zeta, int *it)
-{
+void iterate(double *zeta, int *it) {
     int j;
     double rnorm;
     double norm_temp1, norm_temp2;
@@ -580,8 +544,7 @@ void iterate(double *zeta, int *it)
     //---------------------------------------------------------------------
     norm_temp1 = 0.0;
     norm_temp2 = 0.0;
-    for (j = 0; j < lastcol - firstcol + 1; j++)
-    {
+    for (j = 0; j < lastcol - firstcol + 1; j++) {
         norm_temp1 = norm_temp1 + x[j] * z[j];
         norm_temp2 = norm_temp2 + z[j] * z[j];
     }
@@ -596,8 +559,7 @@ void iterate(double *zeta, int *it)
     //---------------------------------------------------------------------
     // Normalize z to obtain x
     //---------------------------------------------------------------------
-    for (j = 0; j < lastcol - firstcol + 1; j++)
-    {
+    for (j = 0; j < lastcol - firstcol + 1; j++) {
         x[j] = norm_temp2 * z[j];
     }
 }
