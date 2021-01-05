ded2574b03dfbc1abd6de444ffb73d2c3631a503
upupsheep <sandy861003@gmail.com>
2021-01-05 09:22:45 +0000

    fix typo

--- hw6/hostFE.c
+++ hw6/hostFE.c
@@ -11,39 +11,39 @@ void hostFE(int filterWidth, float *filter, int imageHeight, int imageWidth,
     int filterSize = filterWidth * filterWidth;
 
     // create a command queue
-    cl_commandqueue myqueue;
+    cl_command_queue myqueue;
     myqueue = clCreateCommandQueue(context, device, 0, &status);
 
     // create buffers on device
     int image_data_size = imageHeight * imageWidth * sizeof(float);
     int filter_data_size = filterSize * sizeof(float);
-    cl_mem d_ip = clCreateBuffer(context, CL_MEM_READ_ONLY, image_data_size, NULL, &status);
+    cl_mem d_in = clCreateBuffer(context, CL_MEM_READ_ONLY, image_data_size, NULL, &status);
     cl_mem d_filter = clCreateBuffer(context, CL_MEM_READ_ONLY, filter_data_size, NULL, &status);
-    cl_mem d_op = clCreateBuffer(context, CL_MEM_WRITE_ONLY, image_data_size, NULL, &status);
+    cl_mem d_out = clCreateBuffer(context, CL_MEM_WRITE_ONLY, image_data_size, NULL, &status);
 
     // transfer input data to the device
-    status = clEnqueueWriteBuffer(myqueue, d_ip, CL_TRUE, 0, image_data_size, inputImage, 0, NULL, NULL);
+    status = clEnqueueWriteBuffer(myqueue, d_in, CL_TRUE, 0, image_data_size, inputImage, 0, NULL, NULL);
     status = clEnqueueWriteBuffer(myqueue, d_filter, CL_TRUE, 0, filter_data_size, filter, 0, NULL, NULL);
 
     // create the OpenCL kernel
     cl_kernel kernel = clCreateKernel(program, "convolution", &status);
 
     // set arguments of the kernel
-    clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&d_ip);
-    clSetKernelArg(kernel, 1, sizeof(cl_mem), (void *)&d_op);
+    clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&d_in);
+    clSetKernelArg(kernel, 1, sizeof(cl_mem), (void *)&d_out);
     clSetKernelArg(kernel, 2, sizeof(cl_mem), (void *)&d_filter);
     clSetKernelArg(kernel, 3, sizeof(cl_int), (void *)&imageWidth);
     clSetKernelArg(kernel, 4, sizeof(cl_int), (void *)&filterWidth);
 
     // set local and global workgroup sizes
     size_t localws = {16, 16};
-    size_t globalws = {ImageWidth, ImageHeight};
+    size_t globalws = {imageWidth, imageHeight};
 
     // execute kernel
     clEnqueueNDRangeKernel(myqueue, kernel, 2, 0, globalws, localws, 0, NULL, NULL);
 
     // copy results from device back to host
-    clEnqueueReadBuffer(context, d_op, CL_TRUE, 0, image_data_size, outputImage, NULL, NULL, NULL); // CL_TRUE: blocking read back
+    clEnqueueReadBuffer(context, d_out, CL_TRUE, 0, image_data_size, outputImage, NULL, NULL, NULL); // CL_TRUE: blocking read back
 
     // clean up
     status = clFlush(myqueue);
